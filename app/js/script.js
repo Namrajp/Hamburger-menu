ac7577f81b5e1cd224a7538492ede70bcbea6791
Nammaraj <active99raj@gmail.com>
2021-01-27 21:11:54 +1100

    Mobile menu added- classes in js added

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,26 @@
 const hamburg_btn  = document.getElementById('hamburg-btn');
+const overlay  = document.getElementById('overlay');
+const header = document.querySelector('header');
+
 hamburg_btn.addEventListener('click', function() {
     if (hamburg_btn.classList.contains('open')) {
         hamburg_btn.classList.remove('open')
     }
     else {
         hamburg_btn.classList.add('open');
+        
+
     }
 })
+hamburg_btn.addEventListener('click', function() {
+    if(overlay.classList.contains('header__overlay') ) {
+        overlay.classList.remove('header__overlay');
+        header.classList.add('mobile');
+    }
+    else {
+        overlay.classList.add('header__overlay');
+        header.classList.remove('mobile');
+
+    }
+})
+
