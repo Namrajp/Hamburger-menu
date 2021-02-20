bedda74499da1553d576d727bf3dc51040f605a3
Nammaraj <active99raj@gmail.com>
2021-02-20 23:12:43 +1100

    Commenting in js  links and margin:auto media link

--- app/js/script.js
+++ app/js/script.js
@@ -24,7 +24,8 @@ hamburg_btn.addEventListener('click', function() {
     }
 })
 
-/* Selecting the Nav menu */
+/* Selecting the Nav menu with pseudo elements before border/ And 
+ list items li are better than just links a for querySelectorAll so, replacing just div>a's using ul>li>a*/
 const list = document.querySelector('.header__links')
 const links = list.querySelectorAll('.nav-link');
 var current = document.getElementsByClassName("active");
