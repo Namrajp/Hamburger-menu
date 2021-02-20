8e9cc2a41e6048dda7e38994c3cfd1d88eed4128
Nammaraj <active99raj@gmail.com>
2021-02-21 01:05:12 +1100

    Added about work contact pages.

--- app/js/script.js
+++ app/js/script.js
@@ -25,7 +25,7 @@ hamburg_btn.addEventListener('click', function() {
 })
 
 /* Selecting the Nav menu with pseudo elements before border/ And 
- list items li are better than just links a for querySelectorAll so, replacing just div>a's using ul>li>a*/
+ list items li are better than just links a for querySelectorAll so, replacing just a's using*/
 const list = document.querySelector('.header__links')
 const links = list.querySelectorAll('.nav-link');
 var current = document.getElementsByClassName("active");
