9312c57cf613b58f8cceea9a84b423fbbee9d4b8
Nammaraj <active99raj@gmail.com>
2021-02-26 23:42:53 +1100

    fixed the header navigation mobile and desktop vew

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,10 @@
-fetch("./header.html")
-  .then(response => {
-    return response.text();
-  })
-  .then(data => {
-    document.querySelector("header").innerHTML = data;
-  });
+// fetch("./header.html")
+//   .then(response => {
+//     return response.text();
+//   })
+//   .then(data => {
+//     document.querySelector("header").innerHTML = data;
+//   });
 
 // fetch("./footer.html")
 //   .then(response => {
@@ -29,20 +29,7 @@ fetch("./header.html")
   //   }
   // })();
 
-// const hamburg_btn = document.getElementById("hamburg_btn");
-// const overlay = document.getElementById("overlay");
 
-// hamburg_btn.addEventListener('click', function() {
-//     if(overlay.classList.contains('header__overlay') ) {
-//         overlay.classList.remove('header__overlay');
-//         header.classList.add('mobile');
-//     }
-//     else {
-//         overlay.classList.add('header__overlay');
-//         header.classList.remove('mobile');
-
-//     }
-// })
 
 /* Selecting the Nav menu with pseudo elements before border/ And 
  list items li are better than just links a for querySelectorAll so, replacing just a's using*/
@@ -63,19 +50,19 @@ fetch("./header.html")
 
 //     })
 // }
-const navbar = document.querySelector('.header');
-const current_location = location.href;
-const nav_links = document.getElementsByClassName('nav-link');
-const this_length = nav_links.length;
-console.log(nav_links);
+// const navbar = document.querySelector('.header');
+// const current_location = location.href;
+// const nav_links = document.getElementsByClassName('nav-link');
+// const this_length = nav_links.length;
+// console.log(nav_links);
 
-for (let p=0; p < this_length; p++) {
+// for (let p=0; p < this_length; p++) {
 
-  if (nav_links[p].href === current_location) {   // href attribute links to of href as location 
-      nav_links[p].className = 'active';
-  }  
-};
-console.log('this_length');
+//   if (nav_links[p].href === current_location) {   // href attribute links to of href as location 
+//       nav_links[p].className = 'active';
+//   }  
+// };
+// console.log('this_length');
 
 
 
@@ -87,6 +74,31 @@ console.log('this_length');
 //  })
 
 // https://www.freecodecamp.org/news/learn-bootstrap-4-in-30-minute-by-building-a-landing-page-website-guide-for-beginners-f64e03833f33/#the-new-features-of-bootstrap-4
+const current_location = location.href;  // URL of curent page as displayed on address bar 
+const links = document.getElementsByClassName('nav-link');  // Array of anchor elements in li
+this_length = links.length;  // length of Array
+for (let p=0; p < this_length; p++) {
+
+    if (links[p].href === current_location) {   // href attribute links to of href as location 
+        links[p].classList.add('active');
+    }
+}
+
+
+const hamburg_btn  = document.getElementById('hamburg-btn');
+const overlay  = document.getElementById('overlay');
+const header = document.querySelector('header');
+
+hamburg_btn.addEventListener('click', function() {
+    if (hamburg_btn.classList.contains('open')) {
+        hamburg_btn.classList.remove('open');
+        header.classList.remove('mobile');
+    }
+    else {
+        hamburg_btn.classList.add('open');
+        header.classList.add('mobile');
+    }
+})
 
 
 
